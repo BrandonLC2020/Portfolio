8eed48c32492e3d11664882c6c53cda92e6b9c4f
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-08-03 10:39:07 -0400

    almost ready for basic deploy to GH pages

--- src/data.ts
+++ src/data.ts
@@ -9,6 +9,12 @@ import BackupIcon from '@mui/icons-material/Backup';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import ThermostatIcon from '@mui/icons-material/Thermostat';
 import QueueMusicIcon from '@mui/icons-material/QueueMusic';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import TagIcon from '@mui/icons-material/Tag';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import QueuePlayNextIcon from '@mui/icons-material/QueuePlayNext';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import FolderSpecialIcon from '@mui/icons-material/FolderSpecial';
 
 export const PROJECTS: Project[] = [
   // TODO: Get images and video links for these projects
@@ -96,4 +102,106 @@ export const PROJECTS: Project[] = [
     phase: 'Implementation',
     githubUrl: 'https://github.com/BrandonLC2020/ListenList-iOS',
   },   
+  {
+    id: 6,
+    title: 'March Madness Stats',
+    cardIcon: SportsBasketballIcon,
+    // TODO: Update inspiration and description
+    inspiration: 'I wanted to create a tool that helps developers easily inspect and debug their OAuth redirect URIs. This tool aims to simplify the process of verifying and testing redirect URIs for various applications.',
+    shortDescription: 'A web app for inspecting and testing OAuth redirect URIs.',
+    longDescription: 'This web application allows developers to input their OAuth redirect URIs and inspect the parameters being sent during the authentication process. It provides a user-friendly interface for testing and debugging redirect URIs, making it easier to identify issues and ensure proper functionality.',
+    technologies: ['Java', 'Maven','Spring Boot', 'Docker', 'React', 'TypeScript', 'Material UI', 'College Basketball Data API'],
+    // images: [
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_2.png',
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_3.png'
+    // ],
+    // videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ', // Placeholder
+    phase: 'Implementation',
+    githubUrl: 'https://github.com/BrandonLC2020/MarchMadnessStats',
+  }, 
+  {
+    id: 7,
+    title: 'Tic-Tac-Toe Game',
+    cardIcon: TagIcon,
+    // TODO: Update inspiration and description
+    inspiration: 'I wanted to create a tool that helps developers easily inspect and debug their OAuth redirect URIs. This tool aims to simplify the process of verifying and testing redirect URIs for various applications.',
+    shortDescription: 'A web app for inspecting and testing OAuth redirect URIs.',
+    longDescription: 'This web application allows developers to input their OAuth redirect URIs and inspect the parameters being sent during the authentication process. It provides a user-friendly interface for testing and debugging redirect URIs, making it easier to identify issues and ensure proper functionality.',
+    technologies: ['Java'],
+    // images: [
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_2.png',
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_3.png'
+    // ],
+    // videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ', // Placeholder
+    phase: 'Implementation',
+    githubUrl: 'https://github.com/BrandonLC2020/TicTacToeGUI',
+  },
+  {
+    id: 8,
+    title: 'Now Playing',
+    cardIcon: PlayCircleIcon,
+    // TODO: Update inspiration and description
+    inspiration: 'I wanted to create a tool that helps developers easily inspect and debug their OAuth redirect URIs. This tool aims to simplify the process of verifying and testing redirect URIs for various applications.',
+    shortDescription: 'A web app for inspecting and testing OAuth redirect URIs.',
+    longDescription: 'This web application allows developers to input their OAuth redirect URIs and inspect the parameters being sent during the authentication process. It provides a user-friendly interface for testing and debugging redirect URIs, making it easier to identify issues and ensure proper functionality.',
+    technologies: ['XCode', 'Swift', 'SwiftUI', 'Spotify iOS SDK'],    
+    // images: [
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_2.png',
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_3.png'
+    // ],
+    // videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ', // Placeholder
+    phase: 'Planning & Requirements Analysis',
+    githubUrl: 'https://github.com/BrandonLC2020/NowPlaying',
+  },
+  {
+    id: 9,
+    title: 'WatchList',
+    cardIcon: QueuePlayNextIcon,
+    // TODO: Update inspiration and description
+    inspiration: 'I wanted to create a tool that helps developers easily inspect and debug their OAuth redirect URIs. This tool aims to simplify the process of verifying and testing redirect URIs for various applications.',
+    shortDescription: 'A web app for inspecting and testing OAuth redirect URIs.',
+    longDescription: 'This web application allows developers to input their OAuth redirect URIs and inspect the parameters being sent during the authentication process. It provides a user-friendly interface for testing and debugging redirect URIs, making it easier to identify issues and ensure proper functionality.',
+    technologies: ['React', 'TypeScript', 'Material-UI','TMDB API', '.NET', 'C#'],
+    // images: [
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_2.png',
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_3.png'
+    // ],
+    // videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ', // Placeholder
+    phase: 'Planning & Requirements Analysis',
+    githubUrl: 'https://github.com/BrandonLC2020/WatchList-React-App',
+  },
+  {
+    id: 10,
+    title: 'Spotify Stats Web App',
+    cardIcon: LibraryMusicIcon,
+    // TODO: Update inspiration and description
+    inspiration: 'I wanted to create a tool that helps developers easily inspect and debug their OAuth redirect URIs. This tool aims to simplify the process of verifying and testing redirect URIs for various applications.',
+    shortDescription: 'A web app for inspecting and testing OAuth redirect URIs.',
+    longDescription: 'This web application allows developers to input their OAuth redirect URIs and inspect the parameters being sent during the authentication process. It provides a user-friendly interface for testing and debugging redirect URIs, making it easier to identify issues and ensure proper functionality.',
+    technologies: ['React', 'TypeScript', 'Material-UI', 'Spotify Web API', 'Ruby', 'AWS', 'AWS Aurora', 'MySQL'],
+    // images: [
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_2.png',
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_3.png'
+    // ],
+    // videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ', // Placeholder
+    phase: 'Planning & Requirements Analysis',
+    githubUrl: 'https://github.com/BrandonLC2020/PersonalSpotifyStatsWebApp',
+  },
+  {
+    id: 11,
+    title: 'Developer Portfolio',
+    cardIcon: FolderSpecialIcon,
+    // TODO: Update inspiration and description
+    inspiration: 'I wanted to create a tool that helps developers easily inspect and debug their OAuth redirect URIs. This tool aims to simplify the process of verifying and testing redirect URIs for various applications.',
+    shortDescription: 'A web app for inspecting and testing OAuth redirect URIs.',
+    longDescription: 'This web application allows developers to input their OAuth redirect URIs and inspect the parameters being sent during the authentication process. It provides a user-friendly interface for testing and debugging redirect URIs, making it easier to identify issues and ensure proper functionality.',
+    technologies: ['React', 'TypeScript', 'Material-UI', 'Vite'],
+    // images: [
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_2.png',
+    //     'https://storage.googleapis.com/maker-suite-images/AI_story_generator_3.png'
+    // ],
+    // videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ', // Placeholder
+    phase: 'Deployment',
+    githubUrl: 'https://github.com/BrandonLC2020/Portfolio',
+  },    
 ];
