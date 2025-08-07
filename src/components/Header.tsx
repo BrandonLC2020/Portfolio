9e7c124525cc00f56dc1982fb5c006fd8b15ae6b
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-08-07 19:10:30 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,6 +29,7 @@ import {
   Person as PersonIcon,
   Apps as AppsIcon,
 } from '@mui/icons-material';
+
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
